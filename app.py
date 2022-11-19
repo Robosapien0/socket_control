ef13661fa07c1ea6c67583f6333f05656de3c61d
Robosapien0 <114105207+Robosapien0@users.noreply.github.com>
2022-11-19 00:57:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os, json
 from lib.socket_controller import SocketController
@@ -13,50 +13,50 @@ sc = SocketController(config)
 
 @route('/')
 def index():
-    return static_file('index.html', root=STATIC_PATH)
+	return static_file('index.html', root=STATIC_PATH)
 
 
 @route('/<filename:re:.*\.(html|js|css)>')
 def index(filename):
-    return static_file(filename, root=STATIC_PATH)
+	return static_file(filename, root=STATIC_PATH)
 
 
 @route('/manifest.appcache')
 def manifest():
-    return static_file('manifest.appcache', root=STATIC_PATH, mimetype='text/cache-manifest')
+	return static_file('manifest.appcache', root=STATIC_PATH, mimetype='text/cache-manifest')
 
 
 @route('/status')
 def status():
-    return sc.state
+	return sc.state
 
 
 @route('/sockets')
 def status():
-    return {
-        1: config.SOCKET_NAMES[0],
-        2: config.SOCKET_NAMES[1],
-        3: config.SOCKET_NAMES[2],
-        4: config.SOCKET_NAMES[3],
-    }
+	return {
+		1: config.SOCKET_NAMES[0],
+		2: config.SOCKET_NAMES[1],
+		3: config.SOCKET_NAMES[2],
+		4: config.SOCKET_NAMES[3],
+	}
 
 
 @route('/<socket:re:([1234]|all)>/<state:re:[01]>')
 def switch_socket(socket, state):
-    """
-    define catch all endpoint for sending signals to sockets like:
-    /1/1  => turn_1_on
-    /1/0  => turn_1_off
-    """
-    method = "switch_%s_%s" % (socket, ('on' if state == '1' else 'off'))
-    getattr(sc, method)()
-    return sc.state
+	"""
+	define catch all endpoint for sending signals to sockets like:
+	/1/1  => turn_1_on
+	/1/0  => turn_1_off
+	"""
+	method = "switch_%s_%s" % (socket, ('on' if state == '1' else 'off'))
+	getattr(sc, method)()
+	return sc.state
 
 
 if __name__ == "__main__":
-    # Init SocketController and Run server
-    sc.start()
-    run(host='0.0.0.0', port=8080)
+	# Init SocketController and Run server
+	sc.start()
+	run(host='0.0.0.0', port=8080)
 
-    # Clean up
-    sc.stop()
+	# Clean up
+	sc.stop()
