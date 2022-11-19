cf5f93f4cc5766c99004682dadae0ec5f0b7a2f1
Robosapien0 <114105207+Robosapien0@users.noreply.github.com>
2022-11-19 01:24:42 +0000

    Updated to python 3

--- lib/pimote.py
+++ lib/pimote.py
@@ -10,67 +10,67 @@ interface for the pimote.
 """
 
 CODES = {
-    '1-on':    [1, 1, 1, 1],  # 7
-    '1-off':   [0, 1, 1, 1],
-    '2-on':    [1, 1, 1, 0],  # 6
-    '2-off':   [0, 1, 1, 0],
-    '3-on':    [1, 1, 0, 1],  # 5
-    '3-off':   [0, 1, 0, 1],
-    '4-on':    [1, 1, 0, 0],  # 4
-    '4-off':   [0, 1, 0, 0],
-    'all-on':  [1, 0, 1, 1],  # 3
-    'all-off': [0, 0, 1, 1],
+	'1-on':		[1, 1, 1, 1],  # 7
+	'1-off':	[0, 1, 1, 1],
+	'2-on':		[1, 1, 1, 0],  # 6
+	'2-off':	[0, 1, 1, 0],
+	'3-on':		[1, 1, 0, 1],  # 5
+	'3-off':	[0, 1, 0, 1],
+	'4-on':		[1, 1, 0, 0],  # 4
+	'4-off':	[0, 1, 0, 0],
+	'all-on':	[1, 0, 1, 1],  # 3
+	'all-off':	[0, 0, 1, 1],
 }
 
 
 def init():
-    # set the pins numbering mode
-    GPIO.setmode(GPIO.BOARD)
-    # # Select the GPIO pins used for the encoder K0-K3 data inputs
-    GPIO.setup(11, GPIO.OUT)
-    GPIO.setup(15, GPIO.OUT)
-    GPIO.setup(16, GPIO.OUT)
-    GPIO.setup(13, GPIO.OUT)
-    # # Select the signal to select ASK/FSK
-    GPIO.setup(18, GPIO.OUT)
-    # # Select the signal used to enable/disable the modulator
-    GPIO.setup(22, GPIO.OUT)
-    # # Disable the modulator by setting CE pin lo
-    GPIO.output(22, False)
-    # # Set the modulator to ASK for On Off Keying
-    # # by setting MODSEL pin lo
-    GPIO.output(18, False)
-    # # Initialise K0-K3 inputs of the encoder to 0000
-    GPIO.output(11, False)
-    GPIO.output(15, False)
-    GPIO.output(16, False)
-    GPIO.output(13, False)
-    pass
+	# set the pins numbering mode
+	GPIO.setmode(GPIO.BOARD)
+	# # Select the GPIO pins used for the encoder K0-K3 data inputs
+	GPIO.setup(11, GPIO.OUT)
+	GPIO.setup(15, GPIO.OUT)
+	GPIO.setup(16, GPIO.OUT)
+	GPIO.setup(13, GPIO.OUT)
+	# # Select the signal to select ASK/FSK
+	GPIO.setup(18, GPIO.OUT)
+	# # Select the signal used to enable/disable the modulator
+	GPIO.setup(22, GPIO.OUT)
+	# # Disable the modulator by setting CE pin lo
+	GPIO.output(22, False)
+	# # Set the modulator to ASK for On Off Keying
+	# # by setting MODSEL pin lo
+	GPIO.output(18, False)
+	# # Initialise K0-K3 inputs of the encoder to 0000
+	GPIO.output(11, False)
+	GPIO.output(15, False)
+	GPIO.output(16, False)
+	GPIO.output(13, False)
+	pass
 
 
 def term():
-    GPIO.cleanup()
-    pass
+	GPIO.cleanup()
+	pass
 
 
 def switch(code):
-    a, b, c, d = [bool(c) for c in CODES[code]]
-    t = datetime.utcnow().replace(tzinfo = pytz.utc)
-    stdout.write('%s : Sending code %d%d%d%d\n' %
-                 (t.isoformat(), int(a), int(b), int(c), int(d)))
-    try:
-        GPIO.output(11, d)
-        GPIO.output(15, c)
-        GPIO.output(16, b)
-        GPIO.output(13, a)
-        # let it settle, encoder requires this
-        time.sleep(0.1)
-        # Enable the modulator
-        GPIO.output(22, True)
-        # keep enabled for a period
-        time.sleep(0.25)
-        # Disable the modulator
-        GPIO.output(22, False)
-        return True
-    except:
-        return False
+	a, b, c, d = [bool(c) for c in CODES[code]]
+	t = datetime.utcnow().replace(tzinfo = pytz.utc)
+	stdout.write('%s : Sending code %d%d%d%d\n' %
+		(t.isoformat(), int(a), int(b), int(c), int(d)))
+	try:
+		GPIO.output(11, d)
+		GPIO.output(15, c)
+		GPIO.output(16, b)
+		GPIO.output(13, a)
+		# let it settle, encoder requires this
+		time.sleep(0.1)
+		# Enable the modulator
+		GPIO.output(22, True)
+		# keep enabled for a period
+		time.sleep(0.25)
+		# Disable the modulator
+		GPIO.output(22, False)
+		return True
+	except:
+		return False
--- lib/scheduler.py
+++ lib/scheduler.py
@@ -8,119 +8,118 @@ from urllib2 import urlopen
 
 
 class Scheduler(object):
-    """
-    A scheduler polls an ical endpoint for events to sync a queue of actions.
-    Actions in the queue are performed via the action_callback, marking the
-    start and end times of the calendar events.
-    Actions are mapped via the action_dict from event names to start and stop
-    commands to be passed to the action_callback at the start and end times of
-    the event.
-    """
+	"""
+	A scheduler polls an ical endpoint for events to sync a queue of actions.
+	Actions in the queue are performed via the action_callback, marking the
+	start and end times of the calendar events.
+	Actions are mapped via the action_dict from event names to start and stop
+	commands to be passed to the action_callback at the start and end times of
+	the event.
+	"""
 
-    def __init__(self, ical_url, action_callback, action_dict,
-                 polling_interval=10, loop_interval=5):
-        ScheduledAction.set_action_func(action_callback)
+	def __init__(self, ical_url, action_callback, action_dict, polling_interval=10, loop_interval=5):
+		ScheduledAction.set_action_func(action_callback)
 
-        self.ical_url = ical_url
-        self.polling_interval = polling_interval
-        self.loop_interval = loop_interval
-        self.action_dict = action_dict
+		self.ical_url = ical_url
+		self.polling_interval = polling_interval
+		self.loop_interval = loop_interval
+		self.action_dict = action_dict
 
-        self._action_queue = []
-        self._queued_actions = {}
+		self._action_queue = []
+		self._queued_actions = {}
 
-    def start(self):
-        self._loop_thread = Thread(target=self._scheduler_loop)
-        self._loop_thread.daemon = True
-        self._alive = True
-        self._loop_thread.start()
+	def start(self):
+		self._loop_thread = Thread(target=self._scheduler_loop)
+		self._loop_thread.daemon = True
+		self._alive = True
+		self._loop_thread.start()
 
-    def stop(self):
-        self._alive = False
+	def stop(self):
+		self._alive = False
 
-    def _fetch_events(self):
-        try:
-            req = urlopen(self.ical_url)
-            c = Calendar(req.read().decode('iso-8859-1'))
-            req.close()
-            return c.events
-        except Exception, e:
-            t = datetime.utcnow().replace(tzinfo = pytz.utc)
-            stdout.write('%s : Failed to load Calendar\n' % t.isoformat())
+	def _fetch_events(self):
+		try:
+			req = urlopen(self.ical_url)
+			c = Calendar(req.read().decode('iso-8859-1'))
+			req.close()
+			return c.events
+		except Exception as e:
+			t = datetime.utcnow().replace(tzinfo = pytz.utc)
+			stdout.write('%s : Failed to load Calendar\n' % t.isoformat())
 
-    def _update_event_queue(self, now):
-        events = self._fetch_events()
-        if events is None:
-            # Failed to load calendar... *shrug*
-            return
-        now = datetime.utcnow().replace(tzinfo = pytz.utc)
-        for e in events:
-            if e.end.datetime >= now:
-                if e.uid in self._queued_actions:
-                    start_action, end_action = self._queued_actions[e.uid]
-                    if (start_action.scheduledTime < now and
-                            e.begin.datetime > now):
-                        # if active event is rescheduled to the future then end
-                        # it immediately and requeue the start action
-                        end_action.execute()
-                        self._action_queue.append(start_action)
-                    if (start_action.scheduledTime != e.begin.datetime or
-                            end_action.scheduledTime != e.end.datetime):
-                        stdout.write('%s : Event rescheduled %s\n' %
-                                     (now.isoformat(), e.uid))
-                        start_action.scheduledTime = e.begin.datetime
-                        end_action.scheduledTime = e.end.datetime
-                elif e.name in self.action_dict:
-                    stdout.write('%s : Found event %s\n' %
-                                 (now.isoformat(), e.uid))
-                    start_action = ScheduledAction(self.action_dict[e.name][0],
-                                                   e.begin.datetime,
-                                                   e.uid)
-                    end_action = ScheduledAction(self.action_dict[e.name][1],
-                                                 e.end.datetime,
-                                                 e.uid)
-                    self._action_queue.append(start_action)
-                    self._action_queue.append(end_action)
-                    self._queued_actions[e.uid] = (start_action, end_action)
-        self._action_queue.sort(key=lambda c: c.scheduledTime, reverse=True)
+	def _update_event_queue(self, now):
+		events = self._fetch_events()
+		if events is None:
+			# Failed to load calendar... *shrug*
+			return
+		now = datetime.utcnow().replace(tzinfo = pytz.utc)
+		for e in events:
+			if e.end.datetime >= now:
+				if e.uid in self._queued_actions:
+					start_action, end_action = self._queued_actions[e.uid]
+					if (start_action.scheduledTime < now and
+							e.begin.datetime > now):
+						# if active event is rescheduled to the future then end
+						# it immediately and requeue the start action
+						end_action.execute()
+						self._action_queue.append(start_action)
+					if (start_action.scheduledTime != e.begin.datetime or
+							end_action.scheduledTime != e.end.datetime):
+						stdout.write('%s : Event rescheduled %s\n' %
+									 (now.isoformat(), e.uid))
+						start_action.scheduledTime = e.begin.datetime
+						end_action.scheduledTime = e.end.datetime
+				elif e.name in self.action_dict:
+					stdout.write('%s : Found event %s\n' %
+								 (now.isoformat(), e.uid))
+					start_action = ScheduledAction(self.action_dict[e.name][0],
+												   e.begin.datetime,
+												   e.uid)
+					end_action = ScheduledAction(self.action_dict[e.name][1],
+												 e.end.datetime,
+												 e.uid)
+					self._action_queue.append(start_action)
+					self._action_queue.append(end_action)
+					self._queued_actions[e.uid] = (start_action, end_action)
+		self._action_queue.sort(key=lambda c: c.scheduledTime, reverse=True)
 
-    def _check_queue(self, now):
-        while (len(self._action_queue) and
-               self._action_queue[-1].scheduledTime <= now):
-            next_action = self._action_queue.pop()
-            if next_action == self._queued_actions[next_action.uid][1]:
-                # if it's the end action
-                del self._queued_actions[next_action.uid]
-            next_action.execute()
+	def _check_queue(self, now):
+		while (len(self._action_queue) and
+			   self._action_queue[-1].scheduledTime <= now):
+			next_action = self._action_queue.pop()
+			if next_action == self._queued_actions[next_action.uid][1]:
+				# if it's the end action
+				del self._queued_actions[next_action.uid]
+			next_action.execute()
 
-    def _scheduler_loop(self):
-        last_poll_time = datetime.utcnow().replace(
-            tzinfo=pytz.utc) - timedelta(hours=1)
-        while self._alive:
-            now = datetime.utcnow().replace(tzinfo=pytz.utc)
-            if (now - last_poll_time).seconds >= self.polling_interval:
-                self._update_event_queue(now)
-            self._check_queue(now)
-            sleep(self.loop_interval)
+	def _scheduler_loop(self):
+		last_poll_time = datetime.utcnow().replace(
+			tzinfo=pytz.utc) - timedelta(hours=1)
+		while self._alive:
+			now = datetime.utcnow().replace(tzinfo=pytz.utc)
+			if (now - last_poll_time).seconds >= self.polling_interval:
+				self._update_event_queue(now)
+			self._check_queue(now)
+			sleep(self.loop_interval)
 
 
 class ScheduledAction(object):
-    def __init__(self, action, scheduledTime, uid):
-        self.action = action
-        self.scheduledTime = scheduledTime
-        self.uid = uid
-        self.canceled = False
+	def __init__(self, action, scheduledTime, uid):
+		self.action = action
+		self.scheduledTime = scheduledTime
+		self.uid = uid
+		self.canceled = False
 
-    @classmethod
-    def set_action_func(cls, actionFunc):
-        cls.actionFunc = actionFunc
+	@classmethod
+	def set_action_func(cls, actionFunc):
+		cls.actionFunc = actionFunc
 
-    def cancel(self):
-        self.canceled = True
+	def cancel(self):
+		self.canceled = True
 
-    def execute(self):
-        if not self.canceled:
-            t = datetime.utcnow().replace(tzinfo=pytz.utc)
-            stdout.write('%s : Performing action %s for %s\n' %
-                         (t.isoformat(), self.action, self.uid))
-            self.actionFunc(self.action)
+	def execute(self):
+		if not self.canceled:
+			t = datetime.utcnow().replace(tzinfo=pytz.utc)
+			stdout.write('%s : Performing action %s for %s\n' %
+						 (t.isoformat(), self.action, self.uid))
+			self.actionFunc(self.action)
